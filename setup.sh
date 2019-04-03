89f66ccb7e2089b6b95a9428bce544ac62b22ca3
poinap <poinap@protonmail.com>
2019-04-03 12:44:39 +0900

    Clean setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ DOT_DIRECTORY="${HOME}/dotfiles"
 REMOTE_URL="git@github.com:akannke/dotfiles.git"
 
 has() {
-		type "$1" > /dev/null 2>&1
+	type "$1" > /dev/null 2>&1
 }
 
 usage() {
-		name=`basename $0`
-		cat <<EOF
+	name=`basename $0`
+	cat <<EOF
 Usage:
 	$name [arguments] [command]
 
@@ -26,24 +26,23 @@ EOF
 }
 
 if [ ! -d ${DOT_DIRECTORY} ]; then
-		echo "Downloading dotfiles..."
-		mkdir ${DOT_DIRECTORY}
+	echo "Downloading dotfiles..."
+	mkdir ${DOT_DIRECTORY}
 
-		if has "git"; then
-				git clone --recursive "${REMOTE_URL}" "${DOT_DIRECTORY}"
-		else
-				curl -fsSLo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
-				tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOT_DIRECTORY}
-				rm -f ${HOME}/dotfiles.tar.gz
-		fi
+	if has "git"; then
+		git clone --recursive "${REMOTE_URL}" "${DOT_DIRECTORY}"
+	else
+		curl -fsSLo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
+		tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOT_DIRECTORY}
+		rm -f ${HOME}/dotfiles.tar.gz
+	fi
 fi
 
-
 vim_init(){
-		if [ ! -d ${HOME}/.vim ]; then
-				echo "Downloading vim-plug"
-				curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-		fi
+	if [ ! -d ${HOME}/.vim ]; then
+		echo "Downloading vim-plug"
+		curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	fi
 }
 
 vim_init
